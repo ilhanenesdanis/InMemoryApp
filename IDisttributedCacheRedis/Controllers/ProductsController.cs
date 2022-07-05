7cc8fc6e239941c2ca9cc3846f20960b00110ec2
ilhan enes daniş <danisenes3@gmail.com>
2022-07-05 21:28:02 +0300

    Add project files.

---
+++ IDisttributedCacheRedis/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace IDisttributedCacheRedis.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _cache;
+        public ProductsController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public IActionResult Index()
+        {
+            //**************************
+            /*Redis Cache Data ve datanin cache süresini ekleme işlemi*/
+            DistributedCacheEntryOptions entryOptions = new DistributedCacheEntryOptions();
+            entryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _cache.SetString("namess","İLHAN ENES DANİŞ", entryOptions);
+            //**************************
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            //**************
+            //cache üzerinden data okuma işlemi
+            string name = _cache.GetString("namess");
+            //**************
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            _cache.Remove("namess");
+            return View();
+        }
+    }
+}
---
+++ InMemoryApp/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace InMemoryApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _cache;
+        public ProductController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            //1.yol
+            //if (String.IsNullOrEmpty(_cache.Get<string>("zaman")))
+            //{
+            //    _cache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+            //2.yol
+            if(!_cache.TryGetValue("zaman",out string zamancache))
+            {
+                _cache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            
+            return View();
+        }
+        public IActionResult Show()
+        {
+            // _cache.Remove("zaman");//cache de bu key değerine sahip tüm dataları siler
+            _cache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+            ViewBag.zaman = _cache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
