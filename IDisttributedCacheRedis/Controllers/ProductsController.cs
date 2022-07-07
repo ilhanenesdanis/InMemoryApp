fac1b9bbfbc868d91882e3b6ba8c1e1d52876710
ilhan enes daniş <danisenes3@gmail.com>
2022-07-07 22:20:45 +0300

    redis cache 1

--- IDisttributedCacheRedis/Controllers/ProductsController.cs
+++ IDisttributedCacheRedis/Controllers/ProductsController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDisttributedCacheRedis.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace IDisttributedCacheRedis.Controllers
 {
@@ -17,7 +22,7 @@ namespace IDisttributedCacheRedis.Controllers
             /*Redis Cache Data ve datanin cache süresini ekleme işlemi*/
             DistributedCacheEntryOptions entryOptions = new DistributedCacheEntryOptions();
             entryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _cache.SetString("namess","İLHAN ENES DANİŞ", entryOptions);
+            _cache.SetString("namess", "İLHAN ENES DANİŞ", entryOptions);
             //**************************
 
             return View();
@@ -35,5 +40,33 @@ namespace IDisttributedCacheRedis.Controllers
             _cache.Remove("namess");
             return View();
         }
+        #region Complex type cache
+        public IActionResult ComplexTypeCache()
+        {
+            _cache.Remove("product:1");
+            //Json serialize edilerek cache işlemi
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Id=1,Name="Klavye",Price=150},
+                new Product(){Id=2,Name="Leptop",Price=12000},
+                new Product(){Id=3,Name="Mouse",Price=10},
+                new Product(){Id=4,Name="Monitör",Price=1500},
+            };
+            //json Olarak Cache katdetme
+            string jsonProduct = JsonConvert.SerializeObject(products);
+            //_cache.SetString("product:1", jsonProduct);
+
+            Byte[] bypeProduct = Encoding.UTF8.GetBytes(jsonProduct);
+            _cache.Set("product:2", bypeProduct);
+            return View();
+        }
+        public IActionResult GetComplexType()
+        {
+            var data = _cache.GetString("product:1");
+            List<Product> p=JsonConvert.DeserializeObject<List<Product>>(data);
+            ViewBag.list = p;
+            return View();
+        }
+        #endregion
     }
 }
